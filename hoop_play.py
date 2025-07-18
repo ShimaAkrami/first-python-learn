a563eff7931c38543f8ea8a042efc9cdb9a36155
shima <shima95aa1@gmail.com>
2025-07-18 22:09:36 +0330

    done

---
+++ hoop_play.py
@@ -0,0 +1,16 @@
+n=0
+
+while n<20:
+    n+=1
+    if n%3==0 and n%5==0:
+        print('hihoop')
+        continue
+    if n%3==0:
+        print("hop")
+        continue
+    if n%5==0:
+        print("hoop")
+        continue
+   
+    print(n)    
+       
