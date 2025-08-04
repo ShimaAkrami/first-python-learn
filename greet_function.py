f33209f690534fdcaa9fa75e7054c1be335c9a9b
shima <shima95aa1@gmail.com>
2025-08-04 19:56:35 +0330

    done

--- greet_function.py
+++ greet_function.py
@@ -1,9 +1,8 @@
-
-
-def greet (Name):
-   
-    print(f"Hello, {Name}!")
-
-
-
-greet(input())   
+def is_positive(a):
+    if a>=0:
+        print("True") 
+    else:
+        print("False")   
+        
+    
+is_positive(int(input()))
